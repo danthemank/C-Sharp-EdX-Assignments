101759b626cfe0a20c227c055667cd48c9cba7bc
danthemank <danthemank@gmail.com>
2016-03-05 17:38:43 -0500

    Created Project and Class Files
    Created Project and Class Files

---
+++ Module Five/Module Five/Program.cs
@@ -0,0 +1,19 @@
+﻿//Daniel Kemper II
+//Module Five Assignment - Classes
+//3.5.2016
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_Five
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
